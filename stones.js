c01eb58744caf4432cbd267b91a826a0e022283f
Michal <michal.burda91@gmail.com>
2023-10-05 13:43:38 +0200

    Update stones.js

--- stones.js
+++ stones.js
@@ -1,89 +1,77 @@
-const canvas = document.createElement('canvas');
-canvas.width = 800;
-canvas.height = 600;
-document.body.appendChild(canvas);
+const canvas = document.getElementById("gameCanvas");
+    const ctx = canvas.getContext("2d");
+    const boxSize = 20;
+    let snake = [{x: 10, y: 10}];
+    let food = {x: Math.floor(Math.random() * 20), y: Math.floor(Math.random() * 20)};
+    let dx = 0;
+    let dy = 0;
 
-const ctx = canvas.getContext('2d');
+    document.addEventListener("keydown", direction);
 
-const player = {
-    x: canvas.width / 2,
-    y: canvas.height - 50,
-    width: 50,
-    height: 30,
-    dx: 5
-};
-
-const obstacles = [];
-const obstacleWidth = 50;
-const obstacleHeight = 30;
-let lastObstacleTime = 0;
-
-function drawPlayer() {
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(player.x, player.y, player.width, player.height);
-}
-
-function createObstacle() {
-    const obstacleX = Math.random() * (canvas.width - obstacleWidth);
-    const obstacle = {
-        x: obstacleX,
-        y: 0 - obstacleHeight,
-        width: obstacleWidth,
-        height: obstacleHeight,
-        dy: 2
-    };
-    obstacles.push(obstacle);
-}
-
-function drawObstacles() {
-    ctx.fillStyle = 'red';
-    for (let obs of obstacles) {
-        ctx.fillRect(obs.x, obs.y, obs.width, obs.height);
-    }
-}
-
-function updateObstacles() {
-    for (let obs of obstacles) {
-        obs.y += obs.dy;
-    }
-}
-
-function checkCollision() {
-    for (let obs of obstacles) {
-        if (
-            player.x < obs.x + obs.width &&
-            player.x + player.width > obs.x &&
-            player.y < obs.y + obs.height &&
-            player.y + player.height > obs.y
-        ) {
-            alert("Przegrana!");
-            document.location.reload();
+    function direction(event) {
+        if (event.keyCode === 37 && dx === 0) { // ArrowLeft
+            dx = -1; dy = 0;
+        }
+        if (event.keyCode === 38 && dy === 0) { // ArrowUp
+            dx = 0; dy = -1;
+        }
+        if (event.keyCode === 39 && dx === 0) { // ArrowRight
+            dx = 1; dy = 0;
+        }
+        if (event.keyCode === 40 && dy === 0) { // ArrowDown
+            dx = 0; dy = 1;
         }
     }
-}
 
-document.addEventListener('keydown', function (event) {
-    if (event.key === 'ArrowRight' && player.x + player.width < canvas.width) {
-        player.x += player.dx;
-    } else if (event.key === 'ArrowLeft' && player.x > 0) {
-        player.x -= player.dx;
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Draw snake
+        for(let i = 0; i < snake.length; i++) {
+            ctx.fillStyle = i === 0 ? "green" : "white";
+            ctx.fillRect(snake[i].x * boxSize, snake[i].y * boxSize, boxSize, boxSize);
+            ctx.strokeStyle = "black";
+            ctx.strokeRect(snake[i].x * boxSize, snake[i].y * boxSize, boxSize, boxSize);
+        }
+
+        // Draw food
+        ctx.fillStyle = "red";
+        ctx.fillRect(food.x * boxSize, food.y * boxSize, boxSize, boxSize);
+
+        // Move snake
+        let snakeX = snake[0].x + dx;
+        let snakeY = snake[0].y + dy;
+
+        // Snake eats the food
+        if(snakeX === food.x && snakeY === food.y) {
+            food = {
+                x: Math.floor(Math.random() * 20),
+                y: Math.floor(Math.random() * 20)
+            };
+        } else {
+            snake.pop(); // remove tail
+        }
+
+        // New head position
+        let newHead = {
+            x: snakeX,
+            y: snakeY
+        };
+
+        // Game over
+        if (snakeX < 0 || snakeY < 0 || snakeX >= canvas.width/boxSize || snakeY >= canvas.height/boxSize || collision(newHead, snake)) {
+            clearInterval(game);
+            alert("Przegrana!");
+        }
+
+        snake.unshift(newHead);
     }
-});
 
-function gameLoop(timestamp) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    if (timestamp - lastObstacleTime > 2000) {
-        createObstacle();
-        lastObstacleTime = timestamp;
+    function collision(head, array) {
+        for (let i = 0; i < array.length; i++) {
+            if (head.x === array[i].x && head.y === array[i].y) return true;
+        }
+        return false;
     }
 
-    drawPlayer();
-    drawObstacles();
-    updateObstacles();
-    checkCollision();
-
-    requestAnimationFrame(gameLoop);
-}
-
-gameLoop(0);
+    let game = setInterval(draw, 150);
